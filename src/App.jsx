b8bb975d744872322e11ae7ac2e02d620ba715af
ThePotato97 <10260415+ThePotato97@users.noreply.github.com>
2023-05-30 11:18:30 +0100

    setup react

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import "./App.css";
+import React from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
